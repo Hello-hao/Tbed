c5b288c4052ac332d2bc8936fd35c97b8d0a3232
hellohao <923453645@qq.com>
2025-01-06 11:00:42 +0800

    :memo:调整部分代码

--- src/main/java/cn/hellohao/TbedApplication.java
+++ src/main/java/cn/hellohao/TbedApplication.java
@@ -43,8 +43,6 @@ public static void main(String[] args) {
     @Bean
     public static BeanFactoryPostProcessor dependsOnPostProcessor() {
         return bf -> {
-            // Let beans that need the database depend on the DatabaseStartupValidator
-            // like the JPA EntityManagerFactory or Flyway
             String[] flyway = bf.getBeanNamesForType(FirstRun.class);
             Stream.of(flyway)
                     .map(bf::getBeanDefinition)
@@ -56,16 +54,11 @@ public static void main(String[] args) {
                     .forEach(it -> it.setDependsOn("databaseStartupValidator"));
         };
     }
-    /**
-     * 文件上传配置
-     * @return
-     */
+
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-        //  单个数据大小
         factory.setMaxFileSize(DataSize.parse("102400KB")); // KB,MB
-        /// 总上传数据大小
         return factory.createMultipartConfig();
     }
 
--- src/main/java/cn/hellohao/utils/GetIPS.java
+++ src/main/java/cn/hellohao/utils/GetIPS.java
@@ -28,7 +28,6 @@ public class GetIPS implements Runnable {
     public static String getIpAddr(HttpServletRequest request){
         String ip = request.getHeader("X-Forwarded-For");
         if(org.apache.commons.lang3.StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)){
-            //多次反向代理后会有多个ip值，第一个ip才是真实ip
             int index = ip.indexOf(",");
             if(index != -1){
                 return ip.substring(0,index);
